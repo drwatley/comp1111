e73d5a3d96486bf86d454e88b0f26feadbae2922
drwatley <atoussi1@my.bcit.ca>
2015-11-30 21:41:09 -0800

    Update EncryptionDriver.java
    Just made it user friendly, cleaned up the errors.

--- LevelThree/EncryptionDriver.java
+++ LevelThree/EncryptionDriver.java
@@ -1,58 +1,25 @@
 package LevelThree;
 
-import java.util.ArrayList;
 import java.util.Scanner;
-
-import LevelTwo.Bitshifter;
 
 public class EncryptDriver {
 
+    /**
+     *  Main driver class that initiates the program. 
+     */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        ArrayList<String> encrypted = new ArrayList<String>();
-        ArrayList<String> decrypted = new ArrayList<String>();
-        String[] holder;
         
+        System.out.println("Please enter a phrase to encrypt.");
+        String input = scan.nextLine();
         
-        //System.out.println("enter a string");
-        //String line = scan.nextLine();
-        //line = Bitshifter.encrypt(line);
-        //System.out.println(line);
-        //System.out.println(Bitshifter.decrypt(line));
+        String encrypted = Bitshifter.encrypt(input);
         
-       
-        while(scan.hasNextLine()) {
-            String line = scan.nextLine();
-            //System.out.println(line);
-            line = Bitshifter.encrypt(line);
-            //System.out.println(line);
-            //line = Bitshifter.decrypt(line);
-            //System.out.println(line);
-            encrypted.add(line);   
-            
-        }
+        System.out.println(encrypted);
         
-        for(String s: encrypted){
-            System.out.println(s);
-        }
+        System.out.println(Bitshifter.decrypt(encrypted));
         
-        holder = new String[encrypted.size()];
+    
         
-        encrypted.toArray(holder);
-        
-        
-        for(int i = 0; i < holder.length; i++) {
-            System.out.println(holder[i]);
-            String line = holder[i];
-            line = Bitshifter.decrypt(line);
-            System.out.println(line);
-            decrypted.add(line);
-           
-        }
-        
-        
-        for(String s: decrypted) {
-            System.out.println(s);
-        }
     }
 }
