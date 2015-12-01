3ba0f03a12f46dfdc1b1098cec8c40701c16e162
drwatley <atoussi1@my.bcit.ca>
2015-11-30 21:50:39 -0800

    Update and rename EncryptionDriver to EncryptionDriver.java
    comments and sech.

--- LevelOne/EncryptionDriver.java
+++ LevelOne/EncryptionDriver.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
-
+/**Drive class for encryption example. */
 public class EncryptDriver {
-
+    /**Main entry point for the program, it enters 
+    and performs the programs main function of encryption.*/
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter a phrase to encrypt.");
